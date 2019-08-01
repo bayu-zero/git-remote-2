253204ef7c8bfc70ba72773f303fbd90d238c9c9
bayu-zero <bayu.ftsp@gmail.com>
2019-08-01 20:06:31 +0700

    menambahakan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quasi, itaque velit corrupti earum cumque tenetur nulla, in unde iusto voluptatem tempore sit at quis fugiat similique illo veritatis cupiditate.</p>
+</body>
+</html>
